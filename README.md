21f02d44d08ec586725f6d109b4f13d147a8b5d0
Jon McManus <due@wxwhatever.com>
2021-09-05 11:07:57 +1000

    Initial creation.

---
+++ README.md
@@ -0,0 +1,3 @@
+# RandomPotion
+
+Provides a function that can convert a potion into a potion randomly. Use: `randompotion:random_potion` as your item function on an item that can have a potion applied to it.
---
+++ src/main/java/noobanidus/mods/randompotion/init/ModLoot.java
@@ -0,0 +1,28 @@
+package mysticmods.mysticalworld.init;
+
+import mysticmods.mysticalworld.MysticalWorld;
+import mysticmods.mysticalworld.loot.conditions.HasHorns;
+import mysticmods.mysticalworld.loot.conditions.IsColor;
+import mysticmods.mysticalworld.loot.conditions.IsLava;
+import mysticmods.mysticalworld.loot.conditions.IsObsidian;
+import mysticmods.mysticalworld.loot.functions.RandomPotion;
+import net.minecraft.loot.LootConditionType;
+import net.minecraft.loot.LootFunctionType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+import noobanidus.libs.noobutil.registry.ConfiguredRegistry;
+
+public class ModLoot {
+  public static final ConfiguredRegistry<LootFunctionType> FUNCTION_REGISTRY = new ConfiguredRegistry<>(MysticalWorld.MODID, Registry.LOOT_FUNCTION_TYPE);
+  public static final ConfiguredRegistry<LootConditionType> CONDITION_REGISTRY = new ConfiguredRegistry<>(MysticalWorld.MODID, Registry.LOOT_CONDITION_TYPE);
+
+  public static final LootFunctionType RANDOM_POTION = FUNCTION_REGISTRY.register("random_potion", new LootFunctionType(new RandomPotion.Serializer()));
+
+  public static final LootConditionType HAS_HORNS = CONDITION_REGISTRY.register("has_horns", new LootConditionType(new HasHorns.Serializer()));
+  public static final LootConditionType IS_COLOR = CONDITION_REGISTRY.register("is_color", new LootConditionType(new IsColor.Serializer()));
+  public static final LootConditionType IS_OBSIDIAN = CONDITION_REGISTRY.register("is_lava", new LootConditionType(new IsObsidian.Serializer()));
+  public static final LootConditionType IS_LAVA = CONDITION_REGISTRY.register("is_obsidian", new LootConditionType(new IsLava.Serializer()));
+
+  public static void load() {
+  }
+}
